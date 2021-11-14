a31769da04980331439df4255c06e75bac8a75a8
Y.Akazawa <yoshi_akazawa_sto@RED-iMac-3.local>
2021-11-14 20:46:08 +0900

    Switch the connection method to dynamoDB between Local and Lambda

--- src/infrastructure/DB.go
+++ src/infrastructure/DB.go
@@ -9,10 +9,17 @@ import (
 )
 
 func NewDB() *dynamo.DB {
-    //DBへ接続
+    //dynamoDBへ接続
+    var db *dynamo.DB
+
     sess := session.Must(session.NewSession())
-    creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
-    db := dynamo.New(sess,aws.NewConfig().WithRegion(os.Getenv("AWS_REGION")).WithCredentials(creds),)
+
+    if os.Getenv("GO_ENV") == "production" {
+      db = dynamo.New(sess, &aws.Config{Region: aws.String("ap-northeast-1")})
+    }else{
+      creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), "")
+      db = dynamo.New(sess,aws.NewConfig().WithRegion(os.Getenv("AWS_REGION")).WithCredentials(creds),)
+    }
 
     return db
 }
